f7f87bce9754b3c40b6744a6c51c9e8f726051a9
Ben-Swindells <benswindells>
2019-07-18 21:33:03 +0100

    Updated Idle DIalogue System

--- Assets/Scripts/AI/NpcController.cs
+++ Assets/Scripts/AI/NpcController.cs
@@ -7,23 +7,46 @@ public class NpcController : MonoBehaviour
 {
     [Header("NPC attributes")]
     public int currencyHeld;
-    public string idleDialogue;
+    public string[] idleDialogue;
     public int health;
 
     [Header("Required Objects")]
     public TMP_Text idleDialogueUI;
 
+    private float timeLeft = 6;
+    private float wait = 2;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
-        idleDialogueUI.text = idleDialogue;
+        idleDialogueUI.text = idleDialogue[Random.Range(0, idleDialogue.Length)];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        IdleDialgoueCounter();
     }
+    void IdleDialgoueCounter()
+    {
+      timeLeft -= Time.deltaTime;
+
+       if(timeLeft < 0)
+      {
+            idleDialogueUI.text = "";
+
+            wait -= Time.deltaTime;
+
+            if(wait < 0)
+            {
+                timeLeft = 6;
+
+                idleDialogueUI.text = idleDialogue[Random.Range(0, idleDialogue.Length)];
+
+                wait = 2;
+            }
+      }
+    }
+
 }
